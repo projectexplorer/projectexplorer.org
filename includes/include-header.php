66af984e7d1801fe1fb16c1eec206aad20258621
Matt Boggie <mboggie@me.com>
2014-03-21 00:13:01 -0400

    Fix for #9 - removed check for 'levels' in the url

--- includes/include-header.php
+++ includes/include-header.php
@@ -3,7 +3,7 @@
             <h1 class="third"><a href="/">ProjectExplorer</a></h1>
             <nav>
                 <a href="/">Home</a>
-                <a href="/explorers/" <?php if($pageLocation == 'levels') { echo 'class="current"'; } ?>>The Explorers</a>
+                <a href="/explorers/">The Explorers</a>
                 <a href="/about/">What We Do</a>
                 <a href="/about/teacher-registration">Stuff for Teachers</a>
                 <a href="/about/donate">Donate</a>
