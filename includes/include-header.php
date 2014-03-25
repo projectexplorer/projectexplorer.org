43c3ac32e3301bf6b8bb7334d18e0248afd434ab
Mike Davis <mikedavis@smartmediacreative.com>
2014-03-24 22:51:15 -0400

    Plumbing for teachers landing page. Closes #6.

--- includes/include-header.php
+++ includes/include-header.php
@@ -5,7 +5,7 @@
                 <a href="/">Home</a>
                 <a href="/explorers/">The Explorers</a>
                 <a href="/about/">What We Do</a>
-                <a href="/about/teacher-registration">Stuff for Teachers</a>
+                <a href="/teachers">Stuff for Teachers</a>
                 <a href="/about/donate">Donate</a>
             </nav>
       </div>
