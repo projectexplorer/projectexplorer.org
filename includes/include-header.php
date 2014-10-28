2fe00732eb8a3457f5103d4b24d5ed7b873b508c
Matt Boggie <mboggie@me.com>
2014-10-27 20:49:48 -0400

    Adding support for 10million page

--- includes/include-header.php
+++ includes/include-header.php
@@ -5,10 +5,11 @@
 
 <header>
 	<div class="wrapper">
-            <h1 class="third"><a href="/">ProjectExplorer</a></h1>
+            <h1 class="quarter"><a href="/">ProjectExplorer</a></h1>
             <nav>
                 <a href="/">Home</a>
                 <a href="/explorers/">The Explorers</a>
+                <a href="/about/10million">#10Million</a>
                 <a href="/about/">What We Do</a>
                 <a class="teacherhighlight" href="/levels/teachers">For Teachers</a>
                 <a href="/about/donate">Donate</a>
