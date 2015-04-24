1158575f89d469caa9349758a48334a95f26a0c3
Matt Boggie <mboggie@me.com>
2015-04-23 21:03:09 -0400

    coding for tags mostly working. now to style.

--- page_teachers.php
+++ page_teachers.php
@@ -37,10 +37,32 @@
             </txp:if_section>
 
         <txp:else />
+            <div class="twothird">
+                <txp:smd_if_tag_list>
+                    <txp:smd_tag_list type="file">
+                        This should show me one tag: <txp:smd_tag_name title="1" />
+                    </txp:smd_tag_list>
+                
+                    <txp:smd_related_tags wraptag="ul" break="li" type="file">
+                        <txp:smd_if_tag type="file">
+                            <txp:file_download_link><txp:file_download_name /></txp:file_download_link>: <txp:file_download_description />
+                        </txp:smd_if_tag>
+                    </txp:smd_related_tags>
+                </txp:smd_if_tag_list>
 
-            <h2>Levels_list</h2>
-            <txp:article form="levels_list" pgonly="0" status="4" />
-            <txp:article_list form="levels_list" status="live" />
+                <txp:smd_if_tag type="file">
+                    In a tag (sort of?)
+                <txp:else />
+                    I'm not in a tag
+                </txp:smd_if_tag>
+            </div>
+            <div class="sidebar sidebar--about third last">
+                <h3>Subjects</h3>
+                <txp:smd_tag_list type="file" showall="1">
+                  <a href="/lessons/file/<txp:smd_tag_name title="0" />"><txp:smd_tag_name title="1" /> <txp:smd_tag_count wrapcount="[:]" /></a>
+                </txp:smd_tag_list>
+            </div>
+
 
         </txp:if_individual_article>
     
